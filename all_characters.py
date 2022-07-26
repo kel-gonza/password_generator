320cba99ca5745818acf3253093de9364269e530
Kelly <kellygonza12@gmail.com>
2022-07-26 16:11:15 -0700

    add all characters function

---
+++ all_characters.py
@@ -0,0 +1,8 @@
+import random
+from variables import lower_letters, upper_letter, symbols, numbers
+
+
+def all_characters(what_kind_pass):
+    # this function will use all characters like special characters, numbers,
+    # upper and lowercase letters
+    pass
