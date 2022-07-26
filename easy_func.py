e1eb7f9849c9fed21cb7b14ad40e24a078ae1ba3
Kelly <kellygonza12@gmail.com>
2022-07-26 16:10:23 -0700

    add easy to say function

---
+++ easy_func.py
@@ -0,0 +1,8 @@
+import random
+from variables import lower_letters, upper_letter
+
+
+def easy_to_say(what_kind_pass):
+    # easy to say function gives a password that has no numbers and no special
+    # characters
+    pass
