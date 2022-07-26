65897326b08d223d5949084692b7001c10d7c448
Kelly <kellygonza12@gmail.com>
2022-07-26 16:10:47 -0700

    add easy to read function

---
+++ easy_read.py
@@ -0,0 +1,8 @@
+import random
+from variables import lower_letters, upper_letter, symbols, numbers
+
+
+def easy_to_read(what_kind_pass):
+    # easy to read function will give no ambiguous characters like:
+    # i, I, l, L, 0, O, |
+    pass
