5127ac6c9772e9fd6e7566490d2e03d38b361c4a
Kelly <kellygonza12@gmail.com>
2022-07-26 17:45:06 -0700

    change numbers into strings and copy all characters into easy functions

--- easy_func.py
+++ easy_func.py
@@ -1,8 +1,15 @@
 import random
 from variables import lower_letters, upper_letter
 
+characters = []
+letters = list(lower_letters + upper_letter)
 
-def easy_to_say(what_kind_pass):
+
+def easy_to_say(what_kind_pass, password_length):
     # easy to say function gives a password that has no numbers and no special
     # characters
-    pass
+    for letters in range(password_length):
+        characters.append(random.choice(letters))
+    random.shuffle(characters)
+    password = ''.join(characters)
+    return password
