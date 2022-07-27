5895fb0478f366011d68d7d4a2ea4dd8425fb19e
Kelly <kellygonza12@gmail.com>
2022-07-26 19:44:51 -0700

    add ambiguous function, gets rid of all ambiguous letters and numbers, makes it easier to read

--- easy_func.py
+++ easy_func.py
@@ -1,15 +1,26 @@
 import random
-from variables import lower_letters, upper_letter
+from variables import lower_letters, upper_letter, symbols, numbers
 
 characters = []
-letters = list(lower_letters + upper_letter)
+
+ambiguous = list(lower_letters + upper_letter + symbols + numbers)
+
+# remove ambiguous letters
+ambiguous.remove('i')
+ambiguous.remove('I')
+ambiguous.remove('l')
+ambiguous.remove('0')
+ambiguous.remove('L')
+ambiguous.remove('O')
+ambiguous.remove('|')
 
 
 def easy_to_say(what_kind_pass, password_length):
-    # easy to say function gives a password that has no numbers and no special
-    # characters
+    # easy to read function will give no ambiguous characters like:
+    # i, I, l, L, 0, O, |
+
     for letters in range(password_length):
-        characters.append(random.choice(letters))
+        characters.append(random.choice(ambiguous))
     random.shuffle(characters)
     password = ''.join(characters)
     return password
